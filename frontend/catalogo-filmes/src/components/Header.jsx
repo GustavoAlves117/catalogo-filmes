388362f3ec3325b4f60ad9ce59d425db858d5bb2
Gustavo Alves Borges <116588880+GustavoAlves117@users.noreply.github.com>
2025-11-29 15:43:58 -0300

    Update Header.jsx

--- frontend/catalogo-filmes/src/components/Header.jsx
+++ frontend/catalogo-filmes/src/components/Header.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import icon from "../assets/icon.png";
 import "./Header.css";
 
 export default function Header({ onAbrirModal }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="header">
+
       <Link to="/" className="logo">
         <img src={icon} className="logo-header" alt="Logo" />
       </Link>
 
-      <nav>
+      <button className="menu-btn" onClick={() => setMenuOpen(!menuOpen)}>
+        ☰
+      </button>
+
+      <nav className={`nav-menu ${menuOpen ? "open" : ""}`}>
         <Link className="header-link" to="/">Home</Link>
         <Link className="header-link" to="/minha-lista">Minha Lista</Link>
         <Link onClick={onAbrirModal} className="header-link btn-abrir-modal">
           Adicionar Filme
         </Link>
       </nav>
+
     </header>
   );
 }
